bc5c3e12d6277365bf774424480ce3ae5393f3b8
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-09-01 16:54:53 +0530

    update problem

--- backend/src/controllers/userProblem.js
+++ backend/src/controllers/userProblem.js
@@ -1,46 +1,104 @@
-import {getLanguageById, submitBatch} from "../utils/problemUtility.js";
+import { getLanguageById, submitBatch } from "../utils/problemUtility.js";
 import Problem from "../models/problem.js";
 
+export const createProblem = async (req, res) => {
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    visibleTestCases,
+    hiddenTestCases,
+    startCode,
+    referenceSolution,
+    problemCreator,
+  } = req.body;
+  try {
+    for (const { language, completeCode } of referenceSolution) {
+      // sorce_code
+      // Language_id
+      // stdin
+      // expected_output
 
-const createProblem = async (req, res) => {
-    const { title, description, difficulty, tags, visibleTestCases, hiddenTestCases, startCode, referenceSolution, problemCreator } = req.body;
-    try {
-        for(const {language, completeCode} of referenceSolution){
-            // sorce_code
-            // Language_id
-            // stdin
-            // expected_output
+      const languageId = getLanguageById(language);
+      // i am creating Batch Submission
+      const submission = visibleTestCases.map((testcase) => ({
+        source_code: completeCode,
+        language_id: languageId,
+        stdin: testcase.input,
+        expected_output: testcase.output,
+      }));
 
-            const languageId = getLanguageById(language);
-            // i am creating Batch Submission
-            const submission = visibleTestCases.map((testcase) => ({
-                source_code:completeCode,
-                language_id: languageId,
-                stdin: testcase.input,
-                expected_output: testcase.output
-            }));
+      const submitResult = await submitBatch(submission);
 
-            const submitResult = await submitBatch(submission);
+      const resultToken = submitResult.map((value) => value.token);
+      const testResult = await submitToken(resultToken);
 
-            const resultToken = submitResult.map((value)=> value.token);
-            const testResult = await submitToken(resultToken);
-
-            for (const test of testResult) {
-                if (test.status.id != 3){
-                    return res.status(400).send("Error Occured");
-                }
-            }
+      for (const test of testResult) {
+        if (test.status.id != 3) {
+          return res.status(400).send("Error Occured");
         }
-        // we can store it in our DB
-        const userProblem = await Problem.create({
-            ...req.body,
-            problemCreator: req.result._id
-        });
-        res.status(201).json({ message: "Problem Saved Successfully" });
-    } 
-    catch (error) {
-        res.status(500).json({ message: "Error", error });
+      }
     }
-}
+    // we can store it in our DB
+    const userProblem = await Problem.create({
+      ...req.body,
+      problemCreator: req.result._id,
+    });
+    res.status(201).json({ message: "Problem Saved Successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error", error });
+  }
+};
 
-export default createProblem;
+export const updateProblem = async (req, res) => {
+  const { id } = req.params;
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    visibleTestCases,
+    hiddenTestCases,
+    startCode,
+    referenceSolution,
+    problemCreator,
+  } = req.body;
+  try {
+    if(!id) return res.status(400).send("Missing ID field");
+    const DsaProblem = await Problem.findById(id);
+    if(!DsaProblem){
+        return res.status(404).send("Id is not persent in server");
+    }
+    for (const { language, completeCode } of referenceSolution) {
+
+      const languageId = getLanguageById(language);
+      // i am creating Batch Submission
+      const submission = visibleTestCases.map((testcase) => ({
+        source_code: completeCode,
+        language_id: languageId,
+        stdin: testcase.input,
+        expected_output: testcase.output,
+      }));
+
+      const submitResult = await submitBatch(submission);
+
+      const resultToken = submitResult.map((value) => value.token);
+      const testResult = await submitToken(resultToken);
+
+      for (const test of testResult) {
+        if (test.status.id != 3) {
+          return res.status(400).send("Error Occured");
+        }
+      }
+    }
+    // we can store it in our DB
+    const newProblem = await Problem.findByIdAndUpdate(id, {...req.body}, {runValidators: true, new: true});
+    res.status(200).send(newProblem);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
--- backend/src/routes/problem.route.js
+++ backend/src/routes/problem.route.js
@@ -1,18 +1,19 @@
 import express from "express";
 import adminMiddleware from "../middleware/adminMiddleware.js";
-import createProblem from "../controllers/userProblem.js";
+import {createProblem, updateProblem} from "../controllers/userProblem.js";
+import userMiddleware from "../middleware/userMiddleware.js";
 
 
 
 const problemRouter = express.Router();
 
 problemRouter.post("/create",adminMiddleware, createProblem);
-// problemRouter.patch("/:id", updateProblem);
-// problemRouter.delete("/:id", deleteProblem);
+problemRouter.put("/upadte/:id",adminMiddleware, updateProblem);
+problemRouter.delete("/delete/:id",adminMiddleware, deleteProblem);
 
-// problemRouter.get("/:id", getProblemById);
-// problemRouter.get("/", getAllProblem);
-// problemRouter.get("/user", solvedAllProblemByUser);
+problemRouter.get("/problemById/:id",userMiddleware, getProblemById);
+problemRouter.get("/getAllProblem",userMiddleware, getAllProblem);
+problemRouter.get("/problemSolvedByUser",userMiddleware, solvedAllProblemByUser);
 
 
 export default problemRouter;
